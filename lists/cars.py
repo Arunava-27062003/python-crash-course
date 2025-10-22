2bcbb83cdb072b78a3704b6b84b15b6ededbac0e
Arunava-27062003 <arunava.kundu@iemlabs>
2025-10-22 14:36:58 +0530

    list pg 89

--- lists/cars.py
+++ lists/cars.py
@@ -12,4 +12,18 @@ print(cars)
 """ Note: the order of the list 'cars' is permanently changed with 'sort()' method """
 
 """Sorting a List Temporarily with the sorted() Function"""
-# page 87
+
+new_cars = ['tata', 'suzuki', 'ferrari', 'porsche']
+print("\n Temporary Sorted List of New Cars: ")
+print(sorted(new_cars))
+print(f"Actual new cars list {new_cars}")
+
+"""printing a list in reverse order"""
+print("New cars list in reverse order")
+new_cars.reverse()
+print(new_cars)
+
+"""finding the length of a list """
+print(len(cars))
+
+"""pg 89"""
