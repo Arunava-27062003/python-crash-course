d5d1afa1f3e0ed48ec85ef12dc915f364220b549
Arunava-27 <drarunkundu22@gmail.com>
2026-02-20 13:13:30 +0530

    Remove unnecessary .idea configuration files and add new scripts for list operations, including player slicing and list comprehensions.

--- lists/SimpleStatistics.py
+++ lists/SimpleStatistics.py
@@ -4,5 +4,3 @@ data_digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 ans = min(data_digits)
 print(ans)
 print(sum(data_digits))
-
-"""page 105 List comprehensions"""
---
+++ lists/players.py
@@ -0,0 +1,28 @@
+"""
+Working with Part of a List
+
+"""
+
+"""
+# Slicing a List
+"""
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+team = ['team 1', 'team 2']
+
+print(players[0:3]) # start at index 0 and stop at index 3 (not including index 3)
+
+print(players[1:4]) # start at index 1 and stop at index 4 (not including index 4)
+
+print(players[:4]) # start at the beginning of the list
+
+print(players[2:]) # start at index 2 and continue to the end of the list
+
+print(players[-3:]) # start at the third-last item and continue to the end of the list
+
+print(team[-1:])
+
+"""
+page 108, Looping Through a Slice
+"""
